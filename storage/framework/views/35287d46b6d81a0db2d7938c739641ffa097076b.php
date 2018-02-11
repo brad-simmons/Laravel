b4b17a8cfa645e2c06ac6a02137968c970e08d32
Brad_Simmons <Brad_Simmons@cox.net>
2018-02-11 17:02:02 -0500

    initial commit

---
+++ storage/framework/views/35287d46b6d81a0db2d7938c739641ffa097076b.php
@@ -0,0 +1,52 @@
+<?php $__env->startSection('content'); ?>
+<div class="row">
+      <div class="medium-12 large-12 columns">
+        <h4>Clients</h4>
+        <div class="medium-2  columns"><a class="button hollow success" href="<?php echo e(route('new_client')); ?>}">ADD NEW CLIENT</a></div>
+
+<pre><?php echo e(var_dump($clients)); ?></pre>
+        
+        <table class="stack">
+          <thead>
+            <tr>
+              <th width="200">Name</th>
+              <th width="200">Email</th>
+              <th width="200">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+
+              <tr>
+                <td>Mr. Roy Adams</td>
+                <td>roy@email.com</td>
+                <td>
+                  <a class="hollow button" href="./clients_new.html">EDIT</a>
+                  <a class="hollow button warning" href="./book_room.html">BOOK A ROOM</a>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Mr. John Doe</td>
+                <td>john@email.com</td>
+                <td>
+                  <a class="hollow button" href="./clients_new.html">EDIT</a>
+                  <a class="hollow button warning" href="./book_room.html">BOOK A ROOM</a>
+                </td>
+              </tr>
+                            <tr>
+                <td>Ms. Jane Doe</td>
+                <td>jane@email.com</td>
+                <td>
+                  <a class="hollow button" href="./clients_new.html">EDIT</a>
+                  <a class="hollow button warning" href="./book_room.html">BOOK A ROOM</a>
+                </td>
+              </tr>
+              
+                      </tbody>
+        </table>
+
+        
+      </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
